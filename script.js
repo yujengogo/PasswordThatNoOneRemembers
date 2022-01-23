69766934fb896a5e80d87825c3386f391bd4b8de
Yujen Chen <yujengogo@gmail.com>
2022-01-22 23:44:26 -0500

    adding written code to HW pre-set

--- script.js
+++ script.js
@@ -1,60 +1,62 @@
-function passwordGen() {
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-    var choiceLength = window.prompt("Length of the password");
+generateBtn.addEventListener("click", writePassword);
 
-    if (choiceLength <8 || choiceLength > 128) {
-        window.alert("please choose a number between 8-128")
-        return
-    }
-    var choiceLoCase = window.prompt("Lower case? Y/N");
+function writePassword() {
+  var passwordText = document.querySelector("#password");
 
-    var choiceUpCase = window.prompt("Upper case? Y/N");
+  var choiceLength = window.prompt("Length of the password");
 
-    var choiceNumber = window.prompt("Numbers? Y/N");
+  if (choiceLength <8 || choiceLength > 128) {
+      window.alert("please choose a number between 8-128")
+      return
+  }
+  var choiceLoCase = window.prompt("Lower case? Y/N");
 
-    var choiceSpecSim = window.prompt("Symbles? Y/N");
+  var choiceUpCase = window.prompt("Upper case? Y/N");
 
+  var choiceNumber = window.prompt("Numbers? Y/N");
 
+  var choiceSpecSim = window.prompt("Symbles? Y/N");
 
-    const keys = {
-        loCaseChars :
-        "abcdefghijklmnopqrstuvwxyz",
-        upCaseChars :
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-        numbers :
-        "1234567890",
-        specSim :
-        "!@#$%^&*()" 
-    }
+  const keys = {
+      loCaseChars :
+      "abcdefghijklmnopqrstuvwxyz",
+      upCaseChars :
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+      numbers :
+      "1234567890",
+      specSim :
+      "!@#$%^&*()" 
+  }
 
-    let passwordOptions = ''
-        if (choiceLoCase.toLowerCase() == 'y'){
-            [passwordOptions += keys.loCaseChars]
-        }
-        if (choiceUpCase.toLowerCase() == 'y'){
-            [passwordOptions += keys.upCaseChars]
-        }
-        if (choiceNumbers.toLowerCase() == 'y'){
-            [passwordOptions += keys.numbers]
-        }
-        if (choiceSpecSim.toLowerCase() == 'y'){
-            [passwordOptions += keys.specSim]
-        }
+  let passwordOptions = ''
+  if (choiceLoCase.toLowerCase() == 'y'){
+      [passwordOptions += keys.loCaseChars]
+  }
+  if (choiceUpCase.toLowerCase() == 'y'){
+      [passwordOptions += keys.upCaseChars]
+  }
+  if (choiceNumber.toLowerCase() == 'y'){
+      [passwordOptions += keys.numbers]
+  }
+  if (choiceSpecSim.toLowerCase() == 'y'){
+      [passwordOptions += keys.specSim]
+  }
 
-        console.log(passwordOptions)
+  console.log(passwordOptions)
 
-        let password = ''
+  let password = ''
 
-        for (var i = 0; i < choiceLength; i++ ) {
-            var randomChar = passwordOptions[Math.floor(Math.random() * passwordOptions.length)]
-                password += randomChar
-        }
-        console.log(password)
+  for (var i = 0; i < choiceLength; i++ ) {
+      var randomChar = passwordOptions[Math.floor(Math.random() * passwordOptions.length)]
+      password += randomChar
+  }
+  console.log(passwordText)
 
-        window.alert(`your password is
-        ${password}`)
-
-
-
+ passwordText.innerHTML = password;
 
 }
+
+
