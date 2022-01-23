88a2aa445a683895c574c12ce3fdc604bed3c93a
Yujen Chen <yujengogo@gmail.com>
2022-01-22 21:13:44 -0500

    adding var prompt for user's input

---
+++ script.js
@@ -0,0 +1,13 @@
+function passwordGen() {
+
+    var choiceLength = window.prompt("Length of the password");
+
+    var choiceLoCase = window.prompt("Lower case? Y/N");
+
+    var choiceUpCase = window.prompt("Upper case? Y/N");
+
+    var choiceNumber = window.prompt("Numbers? Y/N");
+
+    var choiceSpecSim = window.prompt("Symbles? Y/N");
+
+}
